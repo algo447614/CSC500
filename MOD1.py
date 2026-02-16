5730f9c3eecfa8b39dcc4744ba644a386ef896cc
algo447614 <147664969+algo447614@users.noreply.github.com>
2026-02-16 09:43:18 -0500

    Update MOD1.py

--- MOD1.py
+++ MOD1.py
@@ -1,15 +1,14 @@
-/* 
-Part 1:
+## Part 1:
+##
+## Write a Python program to find the addition and subtraction of two numbers.
+## Ask the user to input two numbers (num1 and num2). Given those two numbers, add them together to find the output. Also, subtract the two numbers to find the output. 
+## 
 
-Write a Python program to find the addition and subtraction of two numbers.
-Ask the user to input two numbers (num1 and num2). Given those two numbers, add them together to find the output. Also, subtract the two numbers to find the output. 
-*/
+##
+## Pseudocode: This program will prompt the user to input one integer, and validate the data type input by user. 
+## Once verified to be an integer, program will repeat in order to intake a second integer. 
+## After two integers are input and verified, then the program will produce both the sum and the difference of the two integers. 
 
-/*
-Pseudocode: This program will prompt the user to input one integer, and validate the data type input by user. 
-Once verified to be an integer, program will repeat in order to intake a second integer. 
-After two integers are input and verified, then the program will produce both the sum and the difference of the two integers. 
-*/
 
 while True:
     try:
