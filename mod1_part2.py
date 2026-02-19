3b8a64c02f02d6703accb7ad049cef26ea62caf3
algo447614 <147664969+algo447614@users.noreply.github.com>
2026-02-18 19:52:35 -0500

    Create mod1_part2.py

---
+++ mod1_part2.py
@@ -0,0 +1,31 @@
+## Part 1:
+##
+## Write a Python program to find the multiplication and division of two numbers.
+## Ask the user to input two numbers (num1 and num2). Given those two numbers, multiply them together to find the output. Also, divide the two numbers to find the output. 
+## 
+
+##
+## Pseudocode: This program will prompt the user to input one integer, and validate the data type input by user. 
+## Once verified to be an integer, program will repeat in order to intake a second integer. 
+## After two integers are input and verified, then the program will produce both the product and the dividend of the two integers. 
+
+
+while True:
+    try:
+        num1 = int(input("Please enter the first number: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+while True:
+    try:
+        num2 = int(input("Please enter the second number: "))
+        break
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+
+product_result = num1 * num2
+dividend_result = num1 / num2
+
+print("Product of numbers:", product_result)
+print("Dividend of numbers:", dividend_result)
